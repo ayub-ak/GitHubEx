086896dc05a3ca3659c80cc6b45661110db3b9c0
AyubKhan <AyubKhan@LAPTOP-ST268433>
2017-12-20 20:36:03 +0530

    Initial commit

---
+++ GitHubEx/src/basicPrograms/ArraysSorting.java
@@ -0,0 +1,26 @@
+package basicPrograms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysSorting 
+{
+	public static void main(String[] args) 
+	{
+		String[] mons ={"jan","feb","mar","apr","may","jun","jul"};
+		System.out.println("Months before sorting : ");
+		printStringArray(mons);
+		Arrays.sort(mons);
+		System.out.println("Months after sorting : ");
+		printStringArray(mons);
+	}
+	
+	public static void printStringArray(String[] mons)
+	{
+		for(String s : mons)
+		{
+			System.out.println(s);
+		}
+	}
+}
